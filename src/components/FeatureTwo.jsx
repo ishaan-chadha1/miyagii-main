ba7e6e3c9e0ea8897c81c6ddd268c9a2c71ffedf
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-04-21 16:26:56 +0530

    Change of Pricing of Our Products Button in All three places

--- src/components/FeatureTwo.jsx
+++ src/components/FeatureTwo.jsx
@@ -83,48 +83,9 @@ function FeatureTwo() {
         </div>
       )}{" "}
       <div className="w-full flex justify-center">
-        <NavLink to="/storage-pricing">
-          <div class="w-full h-40 flex items-center justify-center cursor-pointer">
-            <div class="relative inline-flex items-center justify-start py-2 pl-3 pr-10 overflow-hidden font-semibold shadow transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-white text-black dark:hover:text-gray-200 group">
-              <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
-              <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  fill="none"
-                  class="w-5 h-5 text-green-400"
-                >
-                  <path
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                    stroke-width="2"
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                  ></path>
-                </svg>
-              </span>
-              <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  fill="none"
-                  class="w-5 h-5 text-green-400"
-                >
-                  <path
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                    stroke-width="2"
-                    stroke-linejoin="round"
-                    stroke-linecap="round"
-                  ></path>
-                </svg>
-              </span>
-              <span class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-transparent text-black dark:hover:text-gray-200 group">
-                Pricing of Our Products
-              </span>
-            </div>
-          </div>
-        </NavLink>
+        <button class="cursor-pointer text-white relative text-[14px] w-[9em] h-[3em] text-center bg-gradient-to-r from-violet-500 from-10% via-sky-500 via-30% to-pink-500 to-90% bg-[length:400%] rounded-[30px] z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:from-10% before:via-sky-500 before:via-30% before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-xl before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:10%] active:bg-violet-700 focus:ring-violet-700">
+          Pricing of Our Products
+        </button>
       </div>
     </section>
   );
