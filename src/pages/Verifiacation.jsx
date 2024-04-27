93d5056be04fd5658347b03a642557e445053861
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-04-27 10:28:09 +0530

    Change in Stlying of Pricing of Products button

--- src/pages/Verifiacation.jsx
+++ src/pages/Verifiacation.jsx
@@ -18,9 +18,9 @@ function Verfiaction() {
     <Description />
     {/* <Upload/> */}
     <div className="w-full flex justify-center">
-    <button class="cursor-pointer text-white relative text-[14px] w-[9em] h-[3em] text-center bg-gradient-to-r from-blue-500 from-10% via-blue-600 via-30% to-blue-700 to-90% bg-[length:400%] rounded-[30px] z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-blue-500 before:from-10% before:via-blue-600 before:via-30% before:to-blue-700 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-xl before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:10%] active:bg-blue-800 focus:ring-blue-800">
-        Pricing of Products
-    </button>
+    <button class="cursor-pointer text-white relative text-[14px] w-[10em] h-[3em] text-center bg-gradient-to-r from-blue-500 from-10% via-blue-600 via-30% to-blue-700 to-90% bg-[length:400%] rounded-md z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[2px] before:-bottom-[2px] before:-left-[2px] before:-right-[2px] before:bg-gradient-to-r before:from-blue-500 before:from-10% before:via-blue-600 before:via-30% before:to-blue-700 before:bg-[length:400%] before:-z-10 before:rounded-md before:hover:blur-sm before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:100%] active:bg-blue-800 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+    Pricing of Products
+</button>
 </div>
     <Contact/>
     </div>
