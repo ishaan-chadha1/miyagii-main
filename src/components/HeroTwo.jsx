e113cdc2cfb9f8e21ee3522ac46465c078529f29
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-07-19 14:18:17 +0530

    Fix Landing Animation

--- src/components/HeroTwo.jsx
+++ src/components/HeroTwo.jsx
@@ -67,18 +67,21 @@ function HeroTwo() {
       <motion.img className="box box2 absolute top-[75vh] left-[200.47px] hidden md:inline-block md:top-[75vh] md:left-[200.47px] rotate-[-40.33]" src={box2} alt="box" />
       <motion.img className="box box3 absolute top-[80vh] right-[0px] md:hidden xl:inline-block xl:top-[80vh] xl:right-[300.47px] rotate-[-40.33]" src={box3} alt="box" />
       <motion.img className="box box4 absolute top-[3vh] right-[100px] xl:top-[15vh] xl:right-[10.47px] rotate-[-40.33]" src={box4} alt="box4" />
-
       <div className={`pt-[300px] mx-auto ${styles.paddingX}`}>
-        <div>
-          <h1 className={`${styles.heroHead2Text} text-white uppercase text-center`}>
-            <span className="text-black">
-              {"<"}With <span className="text-[#5379FE]">{displayedWord}</span> your<br />
-              documents are safe{">"}
-            </span>
-          </h1>
-        </div>
+      <div>
+        <h1 className={`${styles.heroHead2Text} text-white uppercase text-center`}>
+          <span className="text-black">
+            {"<"}With 
+          </span>
+          <span className="text-[#5379FE]" style={{ display: 'inline-block', minWidth: '250px', textAlign: 'center' }}>
+            {displayedWord}
+          </span>
+          <span className="text-black">
+            your<br />documents are safe{">"}
+          </span>
+        </h1>
       </div>
-
+    </div>
       <div className={`verify-button-display mt-[100px] max-w-6xl mx-auto ${styles.paddingX} flex flex-row items-start`}>
         <div className="verify-button mx-auto flex flex-row gap-[1.5rem]">
           <motion.button className="min-w-[160px] min-h-[50px] font-poppins font-medium cursor-pointer text-[16px] rounded-3xl border-2 p-2 text-white bg-[#5379FE]" whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.9 }} transition={{ type: "spring", stiffness: 400, damping: 17 }}>
