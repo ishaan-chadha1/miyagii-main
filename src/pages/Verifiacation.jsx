1460bf2458ab14c098a40248a260e26efd49b51d
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-07-21 11:37:49 +0530

    Fix Content in Storage and Verification Pages

--- src/pages/Verifiacation.jsx
+++ src/pages/Verifiacation.jsx
@@ -17,7 +17,7 @@ function Verfiaction() {
       <SubscriptionBanner/>
     {/* <Description /> */}
     {/* <Upload/> */}
-    <div
+    {/* <div
             className={`text-center text-black text-[1rem] md:text-[1.2rem] font-normal leading-7 md:leading-7  capitalize`}
           >
             Miyagi Labs pioneers secure and tamper-proof blockchain-based
@@ -39,7 +39,7 @@ function Verfiaction() {
             cost-effective yet premium alternative to traditional E-signature
             solutions, with the added advantage of decentralization and enhanced
             security over centralized counterparts.
-          </div>
+          </div> */}
     <div className="w-full flex justify-center">
     <button class="cursor-pointer text-white relative text-[14px] w-[10em] h-[3em] text-center bg-gradient-to-r from-blue-500 from-10% via-blue-600 via-30% to-blue-700 to-90% bg-[length:400%] rounded-md z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[2px] before:-bottom-[2px] before:-left-[2px] before:-right-[2px] before:bg-gradient-to-r before:from-blue-500 before:from-10% before:via-blue-600 before:via-30% before:to-blue-700 before:bg-[length:400%] before:-z-10 before:rounded-md before:hover:blur-sm before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:100%] active:bg-blue-800 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
     Pricing of Products
