cbf7f5f068d286fb646ec8eec8fca064aa474c46
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-08-18 12:46:14 +0530

    Removal of Text from verification section under products

--- src/pages/Verifiacation.jsx
+++ src/pages/Verifiacation.jsx
@@ -17,7 +17,7 @@ function Verfiaction() {
         <SubscriptionBanner />
         {/* <Description /> */}
         {/* <Upload/> */}
-        <div
+        {/* <div
           className={`mt-[40px] md:mt-[70px] w-full md:max-w-[44rem]  lg:max-w-5xl mx-auto flex flex-row items-start`}
         >
           <div className="mx-auto flex flex-col gap-[1rem]">
@@ -46,7 +46,7 @@ function Verfiaction() {
               counterparts.
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="w-full flex justify-center mt-20">
           <button class="cursor-pointer text-white relative text-[14px] w-[10em] h-[3em] text-center bg-gradient-to-r from-blue-500 from-10% via-blue-600 via-30% to-blue-700 to-90% bg-[length:400%] rounded-md z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[2px] before:-bottom-[2px] before:-left-[2px] before:-right-[2px] before:bg-gradient-to-r before:from-blue-500 before:from-10% before:via-blue-600 before:via-30% before:to-blue-700 before:bg-[length:400%] before:-z-10 before:rounded-md before:hover:blur-sm before:transition-all before:ease-in-out before:duration-[1s] before:hover:bg-[length:100%] active:bg-blue-800 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
             Pricing of Products
