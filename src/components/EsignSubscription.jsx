3d91a0a2895606deccf721b14b7c9ba8479516f4
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-12-01 17:49:28 +0530

    Addition and Initial Setup of Esign Pricing Page

--- src/components/EsignSubscription.jsx
+++ src/components/EsignSubscription.jsx
@@ -1,87 +1,25 @@
-import { styles } from "../styles";
-import { Image1 } from "../assets";
-import { motion } from "framer-motion";
-import { box1, box8, box13 } from "../assets";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Power1 } from "gsap";
-import { useEffect } from "react";
+import React from "react";
+import Header from "../components/Header";
+import AnimatedBackground from "../components/AnimatedBackground";
 import ESignPlans from "./EsignPlans";
 
-gsap.registerPlugin(ScrollTrigger);
-
 function EsignSubscription() {
-  useEffect(() => {
-    gsap.to(".box14", {
-      scrollTrigger: {
-        trigger: ".trigger5",
-        start: "top top",
-        scrub: 1,
-      },
-      x: -500,
-      y: 200,
-      ease: Power1,
-      duration: 2,
-    });
-
-    gsap.to(".box15", {
-      scrollTrigger: {
-        trigger: ".trigger5",
-        start: "top top",
-        scrub: 1,
-      },
-      x: 300,
-      y: 200,
-      ease: Power1,
-      duration: 2,
-    });
-
-    gsap.to(".box16", {
-      scrollTrigger: {
-        trigger: ".trigger5",
-        start: "top top",
-        scrub: 1,
-      },
-      x: 600,
-      y: 200,
-      ease: Power1,
-      duration: 2,
-    });
-  }, []);
-
   return (
-    <section
-      className={`trigger5 w-full relative md:pt-[100px] pb-[100px] min-h-[80vh]  md:min-h-[100vh] mx-auto bg-[]`}
-    >
-      <motion.img
-        className="box box14 absolute top-[3vh] left-[0px] xl:top-[10vh] xl:left-[0px] rotate-[-40.33]  hidden xl:inline-block"
-        src={box1}
-        alt="box"
-      />
-      <motion.img
-        className="box box15 absolute top-[15vh] right-[0px] xl:top-[25vh] xl:right-[0px] rotate-[-40.33]  hidden md:inline-block"
-        src={box8}
-        alt="box"
-      />
-      <motion.img
-        className="box box16 absolute top-[80vh] right-[10vw] xl:top-[80vh] xl:right-[20vw] rotate-[-40.33]  hidden md:inline-block"
-        src={box13}
-        alt="box"
-      />
-      <div className={` max-w-7xl mx-auto ${styles.paddingX} items-start`}>
-        <div>
-          <h2
-            className={`${styles.heroHeadText}  text-white uppercase text-center`}
-          >
-            <span className="text-black">
-              BLOCKCHAIN-BASED ESIGN <br /> SUBSCRIPTION
-            </span>
-          </h2>
+    <AnimatedBackground>
+      <Header />
+      <div className="pt-[8rem] pb-[4rem]">
+        {/* Added a wrapper with min-h-full to allow scrolling */}
+        <div className="min-h-full flex flex-col items-center justify-start">
+          <div className="max-w-7xl mx-auto text-center mb-12 px-4">
+            <h2 className="text-4xl md:text-5xl font-extrabold text-white uppercase leading-tight">
+              Blockchain-Based Esign <br />
+              <span className="text-blue-500">Subscription</span>
+            </h2>
+          </div>
+          <ESignPlans />
         </div>
-       
       </div>
-      <ESignPlans/>
-    </section>
+    </AnimatedBackground>
   );
 }
 
