24fdfd1ce560921517739b24549cf7bd422bf297
Ishaan Chadha <ishaan.chadha12@gmail.com>
2024-12-01 17:35:06 +0530

    Removal of Contact and Unnecesary code from Esign Pricing Page

--- src/pages/EsignPricing.jsx
+++ src/pages/EsignPricing.jsx
@@ -13,14 +13,6 @@ function EsignPricing() {
       <Header />
       <div className="mt-[10rem] md:mt-[0rem]">
         <EsignSubscription />
-        {/* <div className="w-full flex justify-center">
-          <NavLink to="/subscription">
-            <button className="p-2 rounded-lg text-white bg-[#5379FE] text-[15px] font-thin">
-              pricing of the products
-            </button>
-          </NavLink>
-        </div> */}
-        <Contact />
       </div>
     </>
   );
